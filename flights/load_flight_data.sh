d133fdb61a95f902fc5774959b177cabdc94b778
Rob Hedgpeth <rob.hedgpeth@gmail.com>
2020-01-21 16:09:11 -0600

    updates

--- flights/load_flight_data.sh
+++ flights/load_flight_data.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-SCHEMA_DIR=$(readlink -f ./schema)
+# SCHEMA_DIR=$(readlink -f ./schema)
 # create flights database (dropping if exists) with 3 columnstore tables: flights, airports, airlines
-/usr/bin/mysql --defaults-file=/etc/my.cnf -u root -vvv < $SCHEMA_DIR/schema.sql
+# /usr/bin/mysql --defaults-file=/etc/my.cnf -u root -vvv < $SCHEMA_DIR/schema.sql
 # load data into dimension tables airports and airlines.
-/usr/bin/cpimport -m 2 -s ',' -E '"' flights airports -l $SCHEMA_DIR/airports.csv
-/usr/bin/cpimport -m 2 -s ',' -E '"' flights airlines -l $SCHEMA_DIR/airlines.csv
-[root@flight-demo-mdb-cs-single-0 data]# cat 
-load^C
-[root@flight-demo-mdb-cs-single-0 data]# cat load_flight_data.sh 
+# /usr/bin/cpimport -m 2 -s ',' -E '"' columnstore_schema airports -l $SCHEMA_DIR/airports.csv
+# /usr/bin/cpimport -m 2 -s ',' -E '"' columnstore_schema airlines -l $SCHEMA_DIR/airlines.csv
+#[root@flight-demo-mdb-cs-single-0 data]# cat 
+#load^C
+#[root@flight-demo-mdb-cs-single-0 data]# cat load_flight_data.sh 
 #!/bin/bash
 # check for argument, if so use as wildcard for file load match, otherwise load everything
 DATA_DIR=$(readlink -f ./data)
@@ -21,5 +21,5 @@ fi
 # the advantage of this being runnable as a regular user with a root installation.
 for f in $DATA_DIR/$filematch.csv; do
   echo $f
-  /usr/bin/cpimport -m2 -s ',' -E '"' flights flights -l $f
+  /usr/bin/cpimport -m2 -s ',' -E '"' columnstore_schema flights -l $f
 done
